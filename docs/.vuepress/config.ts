739c17c38f9d2691fcd950908c22ef6343e5e7a9
OrganicFish <2827002857@qq.com>
2022-09-12 18:36:51 +0800

    edit

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,10 +1,12 @@
 import { defineUserConfig } from 'vuepress';
+import { viteBundler } from '@vuepress/bundler-vite';
 import { hopeTheme } from 'vuepress-theme-hope';
 
 export default defineUserConfig({
   lang: 'zh-CN',
   title: "Fish's Notes",
   description: '基于 Vuepress 的笔记 blog',
+  base: '/Notes/',
 
   theme: hopeTheme({
     plugins: {
@@ -28,6 +30,14 @@ export default defineUserConfig({
       red: '#f26d6d',
       green: '#3eaf7c',
       orange: '#fb9b5f',
+    },
+  }),
+
+  bundler: viteBundler({
+    viteOptions: {
+      build: {
+        outDir: '../Notes_Dist',
+      },
     },
   }),
 });
