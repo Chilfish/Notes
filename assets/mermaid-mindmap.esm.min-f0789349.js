8215fc340c37541d32dcd4758318d4b4aeb1b5f7
Organic Fish <2827002857@qq.com>
2023-01-08 16:22:28 +0800

    deploy

---
+++ assets/mermaid-mindmap.esm.min-f0789349.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app-c10f3ead.js";import"./framework-cf51392c.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-4dda171c.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
